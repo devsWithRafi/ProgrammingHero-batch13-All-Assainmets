fe3a4b463b9fa4e69ddb74a83eefb9e4b7d1b9f1
devsWithRafi <mrrafiwebdev@gmail.com>
2026-02-23 00:58:09 +0600

    Added status style functionality in Assainment-4

--- Assainment-4/scripts/reuse.js
+++ Assainment-4/scripts/reuse.js
@@ -4,14 +4,24 @@ const getEliment = (id) => {
 };
 
 
+
 const jobCard = (job) => {
     const card = document.createElement('div');
     card.className = 'card bg-base-300 w-full shadow-sm overflow-hidden border border-gray-700 hover:-translate-y-1 transition-all';
     card.setAttribute('job-id', job.id)
 
+    const interviewStatusStyle = 'border-2 border-green-300/30 bg-green-500/40 text-green-300'
+    const rejectedStatusStyle = 'border-2 border-red-300/30 bg-red-500/40 text-red-300'
+
+    const statusStyle = job.status === 'not_applied' ? null : job.status === 'interview' 
+                        ? interviewStatusStyle : rejectedStatusStyle
+
+    const indicatorColor = job.status === 'not_applied' ? null 
+                           : job.status === 'interview' ? 'bg-green-500/50' : 'bg-red-500/50'
 
     card.innerHTML = `
          <div class="card-body overflow-hidden relative">
+            <span class="absolute ${indicatorColor} top-0 left-0 bottom-0 w-1.5"></span>
             <!-- job card top -->
             <div class="flex items-center justify-between">
               <div>
@@ -31,7 +41,7 @@ const jobCard = (job) => {
                 <li class="list-inside">${job.salary}</li>
               </div>
               <!-- job card status -->
-              <span class="badge badge-outline border-2 border-gray-500 text-gray-400 uppercase font-medium rounded-lg p-4 text-[15px]">${job.status.replace('_', ' ')}</span>
+              <span class="${statusStyle} badge badge-outline border-2 border-gray-500 text-gray-400 uppercase font-medium rounded-lg p-4 text-[15px]">${job.status.replace('_', ' ')}</span>
               <p class="text-zinc-400 sm:text-[18px] text-[15px]">${job.description}</p>
             </div>
 
