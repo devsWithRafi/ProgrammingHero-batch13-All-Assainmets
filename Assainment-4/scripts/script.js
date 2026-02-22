60255c8fbbf6b644f21c1b27867c5146e9078daa
devsWithRafi <mrrafiwebdev@gmail.com>
2026-02-23 01:02:11 +0600

    Added filter functionality in Assainment-4

--- Assainment-4/scripts/script.js
+++ Assainment-4/scripts/script.js
@@ -1,9 +1,17 @@
 const jobCardContainer = getEliment('jobCardContainer');
+
+let currentSelectedTab = 'all';
 let jobData = [...allJobData];
+const tabButtons = document.querySelectorAll('.tabButton')
+const availableJob = getEliment('availableJob')
 
 
-const renderJobCards = () => {    
+
+const renderJobCards = () => {
+    const jobDataFiltered = currentSelectedTab === 'all' ? jobData : jobData.filter((j) => j.status === currentSelectedTab);
+    
     jobCardContainer.innerHTML = '';
+
 
     const totalInterview = jobData.filter(j => j.status.toLowerCase() === 'interview').length
     const totalRejected = jobData.filter(j => j.status.toLowerCase() === 'rejected').length
@@ -13,12 +21,31 @@ const renderJobCards = () => {
     getEliment('totalInterview').innerText = totalInterview
     getEliment('totalRejected').innerText = totalRejected
 
-    jobData.forEach((job) => {
+
+    availableJob.innerText = currentSelectedTab === 'all' ? `${totalJobs} jobs` : (currentSelectedTab === 'interview' 
+                             ? `${totalInterview} of ${totalJobs} jobs` : `${totalRejected} of ${totalJobs} jobs`)
+
+
+    jobDataFiltered.forEach((job) => {
         jobCardContainer.appendChild(jobCard(job));
     });
 
+    tabButtons.forEach(button => {
+        button.classList.remove('bg-green-400')
+
+        if(button.getAttribute('data-value') === currentSelectedTab){
+            button.classList.add('bg-green-400')
+        }
+    })
 };
 
+
+tabButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        currentSelectedTab = button.getAttribute('data-value')
+        renderJobCards()
+    })
+})
 
 const changeStatus = (id, newStatus) => {
     if (!newStatus) return;
